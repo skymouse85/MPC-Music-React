6aa50b38c731f493f377d63b1355f20178de4ee0
Mark Pascucci-Clifford <markmclifford@gmail.com>
2025-02-26 21:58:16 -0800

    react-photo-gallery + lightbox updates

--- src/components/PhotoGallery.js
+++ src/components/PhotoGallery.js
@@ -1,19 +1,39 @@
 import { RowsPhotoAlbum } from "react-photo-album";
 import "react-photo-album/rows.css";
 import GalleryItems from "../config/GalleryItems";
-
-const customRenderPhoto = ({ photo, imageProps, wrapperProps }) => (
-    <figure {...wrapperProps} style={{ margin: 0 }}>
-        <img {...imageProps} alt={photo.alt} />
-        <figcaption style={{ fontSize: "0.75em", textAlign: "center", marginTop: "0.25rem", color: "black" }}>
-            Photo by {photo.photographer}
-        </figcaption>
-    </figure>
-);
+import Lightbox from "yet-another-react-lightbox";
+import { Captions } from "yet-another-react-lightbox/plugins";
+import "yet-another-react-lightbox/styles.css";
+import "yet-another-react-lightbox/plugins/captions.css";
+import React from "react";
 
 function PhotoGallery() {
+    const [index, setIndex] = React.useState(-1);
+    const captionsRef = React.useRef(null);
+
     return (
-        <RowsPhotoAlbum photos={GalleryItems} renderPhoto={customRenderPhoto} />
+        <>
+            <RowsPhotoAlbum
+                photos={GalleryItems}
+                onClick={({ index: current }) => setIndex(current)}
+            />
+            {index >= 0 && (
+                <Lightbox
+                    plugins={[Captions]}
+                    index={index}
+                    slides={GalleryItems}
+                    open={index >= 0}
+                    close={() => setIndex(-1)}
+                    captions={{
+                        ref: captionsRef,
+                        descriptionTextAlign: 'center',
+                        descriptionMaxLines: 2
+                    }}
+                    showToggle={true}
+                    portal={{ root: document.body }} // Ensures Lightbox renders at the root level
+                />
+            )}
+        </>
     );
 }
 
--- src/config/GalleryItems.js
+++ src/config/GalleryItems.js
@@ -6,14 +6,16 @@ const GalleryItems = [
         width: 3000,
         height: 2002,
         alt: "Color Close Field",
-        photographer: 'Lenny Gonzalez'
+        photographer: 'Lenny Gonzalez',
+        description: 'Photo by Lenny Gonzalez'
     },
     {
         src: require("../assets/images/galleryPhotos/colorField.jpg"),
         width: 3000,
         height: 2002,
         alt: "Color Field",
-        photographer: 'Lenny Gonzalez'
+        photographer: 'Lenny Gonzalez',
+        description: 'Photo by Lenny Gonzalez'
     },
     // {
     //     src: require("../assets/images/galleryPhotos/colorPortrait-Edit.jpg"),
@@ -27,112 +29,128 @@ const GalleryItems = [
         width: 2002,
         height: 3000,
         alt: "Full Color Portrait",
-        photographer: 'Lenny Gonzalez'
+        photographer: 'Lenny Gonzalez',
+        description: 'Photo by Lenny Gonzalez'
     },
     {
         src: require("../assets/images/galleryPhotos/colorRoad.jpg"),
         width: 2002,
         height: 3000,
         alt: "Color Road",
-        photographer: 'Lenny Gonzalez'
+        photographer: 'Lenny Gonzalez',
+        description: 'Photo by Lenny Gonzalez'
     },
     {
         src: require("../assets/images/galleryPhotos/colorStreeWalk.jpg"),
         width: 3000,
         height: 2002,
         alt: "Color Street Walking",
-        photographer: 'Lenny Gonzalez'
+        photographer: 'Lenny Gonzalez',
+        description: 'Photo by Lenny Gonzalez'
     },
     {
         src: require("../assets/images/galleryPhotos/colorTree.jpg"),
         width: 2002,
         height: 3000,
         alt: "Color Tree Portrait",
-        photographer: 'Lenny Gonzalez'
+        photographer: 'Lenny Gonzalez',
+        description: 'Photo by Lenny Gonzalez'
     },
     {
         src: require("../assets/images/galleryPhotos/grey_closeField.jpg"),
         width: 3000,
         height: 2002,
         alt: "Grey Close Field",
-        photographer: 'Lenny Gonzalez'
+        photographer: 'Lenny Gonzalez',
+        description: 'Photo by Lenny Gonzalez'
     },
     {
         src: require("../assets/images/galleryPhotos/greyField.jpg"),
         width: 3000,
         height: 2002,
         alt: "Grey Field",
-        photographer: 'Lenny Gonzalez'
+        photographer: 'Lenny Gonzalez',
+        description: 'Photo by Lenny Gonzalez'
     },
     {
         src: require("../assets/images/galleryPhotos/greyPortrait.jpg"),
         width: 2002,
         height: 3000,
         alt: "Grey Portrait",
-        photographer: 'Lenny Gonzalez'
+        photographer: 'Lenny Gonzalez',
+        description: 'Photo by Lenny Gonzalez'
     },
     {
         src: require("../assets/images/galleryPhotos/greyRoad.jpg"),
         width: 2002,
         height: 3000,
         alt: "Grey Road",
-        photographer: 'Lenny Gonzalez'
+        photographer: 'Lenny Gonzalez',
+        description: 'Photo by Lenny Gonzalez'
     },
     {
         src: require("../assets/images/galleryPhotos/greyTree.jpg"),
         width: 2002,
         height: 3000,
         alt: "Grey Tree",
-        photographer: 'Lenny Gonzalez'
+        photographer: 'Lenny Gonzalez',
+        description: 'Photo by Lenny Gonzalez'
     },
     {
         src: require("../assets/images/galleryPhotos/walking grey.jpg"),
         width: 3000,
         height: 2002,
         alt: "Walking Grey",
-        photographer: 'Lenny Gonzalez'
+        photographer: 'Lenny Gonzalez',
+        description: 'Photo by Lenny Gonzalez'
     },
     {
         src: require("../assets/images/galleryPhotos/MarkBow.jpg"),
         width: 1600,
         height: 2413,
         alt: "Mark Session Bow",
-        photographer: 'Ginger Fierstein'
+        photographer: 'Ginger Fierstein',
+        description: 'Photo by Ginger Fierstein'
     },
     {
         src: require("../assets/images/galleryPhotos/MarkMarimba.jpg"),
         width: 1624,
         height: 1600,
         alt: "Mark Session Marimba",
-        photographer: 'Ginger Fierstein'
+        photographer: 'Ginger Fierstein',
+        description: 'Photo by Ginger Fierstein'
     },
     {
         src: require("../assets/images/galleryPhotos/MarkMarimba2.jpg"),
         width: 1624,
         height: 1600,
         alt: "Mark Session Marimba 2",
-        photographer: 'Ginger Fierstein'
+        photographer: 'Ginger Fierstein',
+        description: 'Photo by Ginger Fierstein'
     },
     {
         src: require("../assets/images/galleryPhotos/MarkSessionPercussion.jpg"),
         width: 1624,
         height: 1600,
         alt: "Mark Session Percussion",
-        photographer: 'Ginger Fierstein'
+        photographer: 'Ginger Fierstein',
+        description: 'Photo by Ginger Fierstein'
     },
     {
         src: require("../assets/images/galleryPhotos/MarkSessionVibes.jpg"),
         width: 1600,
         height: 2413,
         alt: "Mark Session Vibes",
-        photographer: 'Ginger Fierstein'
+        photographer: 'Ginger Fierstein',
+        description: 'Photo by Ginger Fierstein'
     },
     {
         src: require("../assets/images/galleryPhotos/MarkSessionPiano.jpg"),
         width: 1600,
         height: 2413,
         alt: "Mark Session Piano",
-        photographer: 'Ginger Fierstein'
+        photographer: 'Ginger Fierstein',
+        description: 'Photo by Ginger Fierstein'
     },
 
 ]
